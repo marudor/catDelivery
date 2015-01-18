eee6ec55eea2a439990b25c54f36d49a2e9f5014
marudor <marudor@marudor.de>
2015-01-18 12:07:53 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express'),
+      app = express(),
+      fs = require('fs'),
+      moment = require('moment'),
+      _ = require('lodash'),
+      path = require('path');
+
+app.listen(61182);
+
+var config = require('./config.json');
+config.catDir = path.resolve(config.catDir);
+
+function randomInt(min, max) {
+  return ~~(Math.random() * (max - min)) + min;
+}
+
+app.get('/', (req, res) => {
+  var dir= config.catDir;
+  var current = moment();
+  var start = moment();
+  _.some(config.times, t => {
+    if (current.day() === start.day(t.day).day()) {
+      if (current.hour() >= start.hour(t.start).hour() && current.hour() <= start.hour(t.end).hour()) {
+        dir = t.dir;
+        return true;
+      }
+    }
+  });
+  
+  dir = fs.readdirSync(dir);
+  res.sendFile(config.catDir+'/'+dir[randomInt(0,dir.length-1)], {}, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+    }
+  });
+});
